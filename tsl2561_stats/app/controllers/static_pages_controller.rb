f64634069325e12950d3245b096abb2775ed774a
Naoaki Obiki <obiki.wk@gmail.com>
2016-11-24 00:37:34 +0900

    data uri schema and realtime

--- tsl2561_stats/app/controllers/static_pages_controller.rb
+++ tsl2561_stats/app/controllers/static_pages_controller.rb
@@ -1,10 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
-    @luxes = Lux.order("recorded_at desc").limit(100)
+    @luxes = Lux.order("recorded_at desc").limit(288)
     # raise @luxes.inspect
     # exit
+    #
+    # Graph.where("id = 1").each do |i|
+    Graph.order("updated_at desc").limit(1).each do |i|
+      @graph = i.graph64
+    end
   end
 
-  def help
+  def realtime
   end
 end
