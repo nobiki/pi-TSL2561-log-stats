02bcac515c765f7c9c6833bf09d89215d33089e9
Naoaki Obiki <obiki.wk@gmail.com>
2016-11-23 19:58:23 +0900

    order

--- tsl2561_stats/app/controllers/static_pages_controller.rb
+++ tsl2561_stats/app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @luxes = Lux
+    @luxes = Lux.order("recorded_at desc").limit(100)
+    # raise @luxes.inspect
+    # exit
   end
 
   def help
