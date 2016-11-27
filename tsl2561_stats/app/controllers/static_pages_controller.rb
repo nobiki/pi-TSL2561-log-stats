5fa770a93d704d4f924117aad148f4771e37a028
Naoaki Obiki <obiki.wk@gmail.com>
2016-11-27 21:40:25 +0900

    realtime

--- tsl2561_stats/app/controllers/static_pages_controller.rb
+++ tsl2561_stats/app/controllers/static_pages_controller.rb
@@ -1,3 +1,5 @@
+require "net/http"
+
 class StaticPagesController < ApplicationController
   def home
     @luxes = Lux.order("recorded_at desc").limit(288)
@@ -11,8 +13,15 @@ class StaticPagesController < ApplicationController
   end
 
   def realtime
-    Lux.order("recorded_at desc").limit(1).each do |i|
-      @lux = i.lux
+    # Lux.order("recorded_at desc").limit(1).each do |i|
+    #   @lux = i.lux
+    # end
+
+    # res = Net::HTTP.start("tsl2561.pi.oji.0j0.jp") do |http|
+    res = Net::HTTP.start("192.168.11.31") do |http|
+      http.get "/"
     end
+
+    @lux = res.body
   end
 end
